618829935312a839f44959f1e95d2aaa5fed8a8e
Jan Wacker <wacker.jan@t-online.de>
2020-12-02 19:54:13 +0100

    Projektdateien hinzufügen.

---
+++ Startup/EMailVersender.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Configuration;
+using System.Net;
+using System.Net.Configuration;
+using System.Net.Mail;
+using System.Diagnostics;
+
+namespace Startup
+{
+    public class EMailVersender
+    {
+        /// <summary>
+        /// Versendet eine Email.
+        /// </summary>
+        public static void SendeEmail(string betreff, string nachricht)
+        {
+            Configuration oConfig = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var mailSettings = oConfig.GetSectionGroup("system.net/mailSettings") as MailSettingsSectionGroup;
+
+            if (mailSettings != null)
+            {
+                int port = mailSettings.Smtp.Network.Port;
+                string host = mailSettings.Smtp.Network.Host;
+                string passwort = mailSettings.Smtp.Network.Password;
+                string nutzername = mailSettings.Smtp.Network.UserName;
+
+                string emaiAn = ConfigurationManager.AppSettings.Get("EMail-An");
+                MailMessage mailMessage = new MailMessage(nutzername, emaiAn, betreff, nachricht);
+
+                SmtpClient smtpClient = new SmtpClient()
+                {
+                    Host = host,
+                    Port = port,
+                    Credentials = new NetworkCredential(nutzername, passwort),
+                    EnableSsl = true
+                };
+
+                try
+                {
+                    smtpClient.Send(mailMessage);
+                }
+                catch (Exception ex)
+                {
+                    using (EventLog eventLog = new EventLog("Application"))
+                    {
+                        eventLog.Source = "Application";
+                        eventLog.WriteEntry(ex.Message, EventLogEntryType.Error, 101, 1);
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ Startup/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Startup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string betreff = "Server Neustart erfolgreich";
+
+            var rechnerName = Environment.MachineName;
+            string nachricht = $"Der Rechner [{rechnerName}] wurde neugestartet aktuelle Uhrzeit: '{DateTime.Now}'";
+
+            Console.WriteLine("Neustart Email Sender");
+            EMailVersender.SendeEmail(betreff, nachricht);
+        }
+    }
+}
